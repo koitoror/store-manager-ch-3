c4cfd1fd246acdf45167f2b5da4bd70fa694eb9e
Daniel Kamar <kamarster@gmail.com>
2018-10-25 22:25:23 +0300

    [Bug #161443803] Views fix

--- app/api/v2/views/sales_views.py
+++ app/api/v2/views/sales_views.py
@@ -17,7 +17,7 @@ dict_cursor = conn.dict_cursor
 class ProductList(Resource):
     """Displays a list of all products and lets you POST to add new products."""
 
-    @api.expect(post_products)
+    @api.expect(post_sales)
     @api.doc('adds an product')
     @api.response(201, "Created")
     @token_required
@@ -34,18 +34,18 @@ class ProductList(Resource):
 
         product_name = args["product_name"]
         product_quantity = args["product_quantity"]
-        Product.add_product(cursor, product_name, product_quantity, user_id)
+        Sale.add_product(cursor, product_name, product_quantity, user_id)
         return {"message": "Product added successfully"}, 201
 
     @api.doc("list_products")
     @api.response(404, "Products Not Found")
-    @api.marshal_list_with(products, envelope="products")
+    @api.marshal_list_with(sales, envelope="products")
     @token_required
     @api.doc(security='apikey')
     @api.header('x-access-token', type=str, description='access token')
     def get(self, user_id):
         """List all Products"""
-        products = Product.get_all(dict_cursor, user_id)
+        products = Sale.get_all(dict_cursor, user_id)
         if not products:
             api.abort(404, "No products for user {}".format(user_id))
         return products
@@ -56,21 +56,21 @@ class ProductList(Resource):
 class ProductClass(Resource):
     """Displays a single product item and lets you delete them."""
 
-    @api.marshal_with(products)
+    @api.marshal_with(sales)
     @api.doc('get one product')
     @token_required
     @api.doc(security='apikey')
     @api.header('x-access-token', type=str, description='access token')
     def get(self, user_id, productId):
         """Displays a single Product."""
-        product = Product.get_product_by_id(dict_cursor, productId)
+        product = Sale.get_product_by_id(dict_cursor, productId)
         if product["user_id"] != str(user_id):
             api.abort(401, "Unauthorized to view this product")
         return product
 
 
     @api.doc('updates an product')
-    @api.expect(post_products)
+    @api.expect(post_sales)
     @token_required
     @api.doc(security='apikey')
     @api.header('x-access-token', type=str, description='access token')
@@ -80,14 +80,14 @@ class ProductClass(Resource):
         product_name = args["product_name"]
         product_quantity = args["product_quantity"]
         product = {"product_name": product_name, "product_quantity":product_quantity}
-        product = Product.get_product_by_id(dict_cursor, productId)
+        product = Sale.get_product_by_id(dict_cursor, productId)
 
         invalid_data = validate_update_product(product, args)
 
         if invalid_data:
             return invalid_data
         
-        Product.modify_product(dict_cursor, cursor, args["product_name"], args["product_quantity"], productId, user_id)
+        Sale.modify_product(dict_cursor, cursor, args["product_name"], args["product_quantity"], productId, user_id)
         return {"message": "Updated successfully", "product":product}
 
     @api.doc('deletes an product')
@@ -98,5 +98,5 @@ class ProductClass(Resource):
     def delete(self, user_id, productId):
         """Deletes a single Product."""
 
-        Product.delete_product(dict_cursor, cursor, productId, user_id)
+        Sale.delete_product(dict_cursor, cursor, productId, user_id)
         return {"message": "Product deleted successully"}, 200

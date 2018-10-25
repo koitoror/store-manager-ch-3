04577f0d0f91d1f9df6a3c5cec746b39f4883fe8
Daniel Kamar <kamarster@gmail.com>
2018-10-25 16:20:31 +0300

    [Bug #161443803] Databas import error

--- app/database.py
+++ app/database.py
@@ -20,21 +20,21 @@ class Database(object):
         self.cursor = self.connection.cursor()
         self.dict_cursor = self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    def connect(self, testing=None):
-        # db_uri = os.getenv("TEST_DB_URL") if testing else os.getenv("DATABASE_URL")
-        db_uri = os.getenv("DATABASE_URL")
-        result = urlparse.urlparse(db_uri)    
-        host = result.hostname
-        role = result.username
-        pwd = result.password
-        database = result.path[1:]
+    # def connect(self, testing=None):
+    #     # db_uri = os.getenv("TEST_DB_URL") if testing else os.getenv("DATABASE_URL")
+    #     db_uri = os.getenv("DATABASE_URL")
+    #     result = urlparse.urlparse(db_uri)    
+    #     host = result.hostname
+    #     role = result.username
+    #     pwd = result.password
+    #     database = result.path[1:]
 
-        return psycopg2.connect(
-            database=database,
-            user=role,
-            host=host,
-            password=pwd,
-        )
+    #     return psycopg2.connect(
+    #         database=database,
+    #         user=role,
+    #         host=host,
+    #         password=pwd,
+        # )
 
     # def connect_db(self):
     #     """Method for creating db connection."""
@@ -45,6 +45,16 @@ class Database(object):
     #         self.dict_cursor = self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
     #     except Exception as e:
     #         return {"message": str(e)}
+
+    def connect_db(self, testing=None):
+        """Method for creating db connection."""
+        try:
+            self.connection = psycopg2.connect(database="d4501hveeam6gl", user="hpwkkcsgckkgvk", password="2e5282386e7f2a3e1faef5558102a71b67019dc02844d1b16a5374b8e354be7a", host="ec2-107-22-189-136.compute-1.amazonaws.com", port="5432")
+            self.connection.autocommit = True
+            self.cursor = self.connection.cursor()
+            self.dict_cursor = self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        except Exception as e:
+            return {"message": str(e)}
 
     def create_tables(self):
         tables=(
@@ -88,5 +98,7 @@ class Database(object):
 
 if __name__=="__main__":
     db = Database()
+    db.connect_db()
     db.create_tables()
+
     # db.connect_db()
